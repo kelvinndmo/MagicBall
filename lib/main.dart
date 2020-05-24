51d72154c519e80eb3a7929ece540a79b21b9b79
novak254 <ndemokelvinonkundi@gmail.com>
2020-05-24 23:50:50 +0300

    magic ball challenge

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import "dart:math";
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            backgroundColor: Colors.blue,
+          ),
+          body: MagicBall(),
+        ),
+      ),
+    );
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int imageNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          FlatButton(
+            onPressed: () {
+              setState(() {
+                imageNumber = Random().nextInt(5) + 1;
+              });
+            },
+            child: Image.asset("images/ball$imageNumber.png"),
+          )
+        ],
+      ),
+    );
+  }
+}
